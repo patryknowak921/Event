1a07f6e5a118a7e8bc9bd81b68c337ff039c437d
Patryk <patnow92@wp.pl>
2017-06-08 13:23:55 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var list = document.getElementById('list');
+var button = document.getElementById('addElem');
+var listLength = document.getElementsByTagName('li').length;
+button.addEventListener('click', function() {
+    list.innerHTML += '<li>item ' + listLength + '</li>';
+    listLength++;
+})
